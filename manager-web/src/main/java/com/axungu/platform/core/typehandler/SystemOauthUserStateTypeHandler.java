a492dcc533d3854d895c3bffaa00f3126a8bae71
jtoms.shen <jtoms.shen@gmail.com>
2019-04-23 16:06:21 +0800

    规范命名

--- manager-web/src/main/java/com/axungu/platform/core/typehandler/SystemOauthUserStateTypeHandler.java
+++ manager-web/src/main/java/com/axungu/platform/core/typehandler/SystemOauthUserStateTypeHandler.java
@@ -1,6 +1,6 @@
 package com.axungu.platform.core.typehandler;
 
-import com.axungu.platform.core.enums.UserState;
+import com.axungu.platform.core.model.SystemOauthUserBaseInfo;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
@@ -9,10 +9,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserStateTypeHandler extends BaseTypeHandler<UserState> {
+/**
+ * @author jtoms
+ */
+public class UserStateTypeHandler extends BaseTypeHandler<SystemOauthUserBaseInfo.UserState> {
 
     @Override
-    public UserState getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    public SystemOauthUserBaseInfo.UserState getNullableResult(ResultSet rs, String columnName) throws SQLException {
         // 根据数据库存储类型决定获取类型，本例子中数据库中存放INT类型
         int i = rs.getInt(columnName);
 
@@ -25,7 +28,7 @@ public class UserStateTypeHandler extends BaseTypeHandler<UserState> {
     }
 
     @Override
-    public UserState getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    public SystemOauthUserBaseInfo.UserState getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         // 根据数据库存储类型决定获取类型，本例子中数据库中存放INT类型
         int i = rs.getInt(columnIndex);
         if (rs.wasNull()) {
@@ -37,7 +40,7 @@ public class UserStateTypeHandler extends BaseTypeHandler<UserState> {
     }
 
     @Override
-    public UserState getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    public SystemOauthUserBaseInfo.UserState getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         // 根据数据库存储类型决定获取类型，本例子中数据库中存放INT类型
         int i = cs.getInt(columnIndex);
         if (cs.wasNull()) {
@@ -49,7 +52,7 @@ public class UserStateTypeHandler extends BaseTypeHandler<UserState> {
     }
 
     @Override
-    public void setNonNullParameter(PreparedStatement ps, int i, UserState parameter, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, SystemOauthUserBaseInfo.UserState parameter, JdbcType jdbcType) throws SQLException {
         // baseTypeHandler已经帮我们做了parameter的null判断
         ps.setInt(i, parameter.getCode());
 
@@ -61,7 +64,7 @@ public class UserStateTypeHandler extends BaseTypeHandler<UserState> {
      * @param code 数据库中存储的自定义code属性
      * @return code对应的枚举类
      */
-    private UserState locate(int code) {
-        return UserState.valueOf(code);
+    private SystemOauthUserBaseInfo.UserState locate(int code) {
+        return SystemOauthUserBaseInfo.UserState.valueOf(code);
     }
 }
